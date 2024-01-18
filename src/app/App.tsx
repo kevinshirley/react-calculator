bfa6b43879a495fa49652c9fecc15d04dd0d0acd
Kevin Shirley <kevinxshirley@gmail.com>
2024-01-18 08:54:00 -0500

    Tailwind + App folder structure

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './styles/App.css'
+
+function App() {
+  return (
+    <main className='relative bg-indigo-950 max-w-full h-screen'>
+      <section className='screen'>
+        screen
+      </section>
+      <section className='buttons'>
+        buttons
+      </section>
+    </main>
+  )
+}
+
+export default App
